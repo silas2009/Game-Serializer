6e934a9e40b32ffa8b4c0f5b159b0b42535e33b5
silas2009 <97012583+silas2009@users.noreply.github.com>
2023-01-15 15:18:14 -0600

    Create RetroStudioSurfaceTextures.lua

---
+++ Serializer/RetroStudioSurfaceTextures.lua
@@ -0,0 +1,52 @@
+local Textures = {
+    {
+        Texture = "rbxassetid://1068388907",
+        Surface = Enum.SurfaceType.Studs
+    },
+    {
+        Texture = "rbxassetid://1068388840",
+        Surface = Enum.SurfaceType.Inlet
+    },
+    {
+        Texture = "rbxassetid://1068388980",
+        Surface = Enum.SurfaceType.Weld
+    },
+    {
+        Texture = "rbxassetid://6879428965",
+        Surface = Enum.SurfaceType.Glue
+    },
+    {
+        Texture = "rbxassetid://3671864277",
+        Surface = Enum.SurfaceType.Studs
+    },
+    {
+        Texture = "rbxassetid://3671874741",
+        Surface = Enum.SurfaceType.Inlet
+    },
+    {
+        Texture = "rbxassetid://3671874741",
+        Surface = Enum.SurfaceType.Weld
+    },
+    {
+        Texture = "rbxassetid://4582437742",
+        Surface = Enum.SurfaceType.Glue
+    },
+    {
+        Texture = "rbxassetid://3678878781",
+        Surface = Enum.SurfaceType.Universal
+    },
+    {
+        Texture = "rbxassetid://788435330",
+        Surface = Enum.SurfaceType.Weld
+    },
+    {
+        Texture = "rbxassetid://7027211371",
+        Surface = Enum.SurfaceType.Studs
+    },
+    {
+        Texture = "rbxassetid://4648233669",
+        Surface = Enum.SurfaceType.Universal
+    }
+}
+
+return Textures
