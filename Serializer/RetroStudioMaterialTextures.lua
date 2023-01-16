d890dd625e7f2cf9f7066579ebef4d9dd23e37dc
silas2009 <97012583+silas2009@users.noreply.github.com>
2023-01-16 04:20:34 -0600

    Create RetroStudioMaterialTextures.lua

---
+++ Serializer/RetroStudioMaterialTextures.lua
@@ -0,0 +1,36 @@
+local Textures = {
+    {
+        Texture = "rbxassetid://6288155249",
+        Material = Enum.Material.Slate
+    },
+    {
+        Texture = "rbxassetid://6288045768",
+        Material = Enum.Material.Wood
+    },
+    {
+        Texture = "rbxassetid://6288044299",
+        Material = Enum.Material.Ice
+    },
+    {
+        Texture = "rbxassetid://8261846916",
+        Material = Enum.Material.Grass
+    },
+    {
+        Texture = "rbxassetid://8261913159",
+        Material = Enum.Material.Grass
+    },
+    {
+        Texture = "rbxassetid://7066125190",
+        Material = Enum.Material.CorrodedMetal
+    },
+    {
+        Texture = "rbxassetid://7066124603",
+        Material = Enum.Material.CorrodedMetal
+    },
+    {
+        Texture = "rbxassetid://6288042730",
+        Material = Enum.Material.Concrete
+    }
+}
+
+return Textures
