ada3243fb4da78df74a3b2be9365b9e3d2fa2b81
silas2009 <97012583+silas2009@users.noreply.github.com>
2023-01-16 01:21:19 -0600

    Update Properties.lua

--- Serializer/Properties.lua
+++ Serializer/Properties.lua
@@ -1,29 +1,42 @@
 local module = {}
 
-function addToTable(Table,AddTable)
-	for i,v in pairs(AddTable) do
-		table.insert(Table,v)
-	end
-
-	return Table
-end
-
-module.Properties = {
-	["Instance"] = {
-		"Name",
-		"ClassName"
+local BaseClasses = {
+	GuiObject = {
+		"Position",
+		"Size",
+		"Visible",
+		"BackgroundColor3",
+		"BackgroundTransparency",
+		"BorderSizePixel",
+		"Draggable",
+		"Active",
+		"ZIndex",
+		"BorderColor3",
+		"ClipsDescendants",
+		"SizeConstraint"
+	},
+	GuiButton = {
+		"AutoButtonColor",
+		"Modal",
+		"Selected"
+	},
+	JointInstance = {
+		"C0",
+		"C1",
+		"Enabled"
 	},
 	BasePart = {
-		"Size",
-		"CFrame",
 		"BrickColor",
-		"Color",
 		"Material",
-		"Transparency",
 		"Reflectance",
+		"Transparency",
+		"CFrame",
+		"AssemblyLinearVelocity",
+		"AssemblyAngularVelocity",
 		"Anchored",
 		"CanCollide",
 		"Locked",
+		"Size",
 		"BackSurface",
 		"BottomSurface",
 		"FrontSurface",
@@ -31,33 +44,92 @@ module.Properties = {
 		"RightSurface",
 		"TopSurface"
 	},
-	SpecialMesh = {
-		"MeshId",
-		"MeshType",
-		"Offset",
-		"Scale",
+	ValueBase = {"Value"},
+	["Instance"] = {"Archivable","Name"}
+}
+
+module.Properties = {
+	TextLabel = {
+		"Font",
+		"TextSize",
+		"FontSize",
+		"Text",
+		"TextColor3",
+		"TextScaled",
+		"TextStrokeColor3",
+		"TextStrokeTransparency",
+		"TextTransparency",
+		"TextWrapped",
+		"TextXAlignment",
+		"TextYAlignment"
+	},
+	TextButton = {
+		"Font",
+		"TextSize",
+		"FontSize",
+		"Text",
+		"TextColor3",
+		"TextScaled",
+		"TextStrokeColor3",
+		"TextStrokeTransparency",
+		"TextTransparency",
+		"TextWrapped",
+		"TextXAlignment",
+		"TextYAlignment"
+	},
+	TextBox = {
+		"ClearTextOnFocus",
+		"MultiLine",
+		"Font",
+		"TextSize",
+		"FontSize",
+		"Text",
+		"TextColor3",
+		"TextScaled",
+		"TextStrokeColor3",
+		"TextStrokeTransparency",
+		"TextTransparency",
+		"TextWrapped",
+		"TextXAlignment",
+		"TextYAlignment"
+	},
+	SurfaceGui = {
+		"AlwaysOnTop",
+		"CanvasSize",
+		"Enabled",
+		"Face"
+	},
+	ScreenGui = {
+		"Enabled"
+	},
+	BillboardGui = {
+		"AlwaysOnTop",
+		"Enabled",
+		"ExtentsOffset",
+		"Size",
+		"SizeOffset",
+		"StudsOffset"
+	},
+	Part = {"Shape"},
+	VehicleSeat = {
+		"Disabled",
+		"HeadsUpDisplay",
+		"MaxSpeed",
+		"Steer",
+		"Throttle",
+		"Torque",
+		"TurnSpeed"
+	},
+	Tool = {
+		"GripForward",
+		"GripPos",
+		"GripRight",
+		"GripUp",
+		"ToolTip",
 		"TextureId",
-		"VertexColor"
-	},
-	CylinderMesh = {
-		"Offset",
-		"Scale",
-		"VertexColor"
-	},
-	BlockMesh = {
-		"Offset",
-		"Scale",
-		"VertexColor"
-	},
-	CharacterMesh = {
-		"BaseTextureId",
-		"BodyPart",
-		"MeshId",
-		"OverlayTextureId"
-	},
-	JointInstance = {
-		"C0",
-		"C1"
+		"CanBeDropped",
+		"RequiresHandle",
+		"Enabled"
 	},
 	Texture = {
 		"Color3",
@@ -67,143 +139,17 @@ module.Properties = {
 		"StudsPerTileV",
 		"Texture",
 		"Transparency",
-		"ZIndex",
 		"Face"
 	},
-	Decal = {
-		"Color3",
-		"Texture",
-		"Transparency",
-		"ZIndex",
-		"Face"
-	},
-	Shirt = {
-		"Color3",
-		"ShirtTemplate"
-	},
-	Pants = {
-		"Color3",
-		"PantsTemplate"
-	},
-	ShirtGraphic = {
-		"Color3",
-		"Graphic"
-	},
-	Fire = {
-		"Color",
-		"Enabled",
-		"Heat",
-		"SecondaryColor",
-		"Size",
-		"TimeScale"
-	},
-	Sparkles = {
-		"Enabled",
-		"SparkleColor",
-		"TimeScale"
-	},
-	Smoke = {
-		"Color",
-		"Enabled",
-		"Opacity",
-		"RiseVelocity",
-		"Size",
-		"TimeScale"
-	},
-	GuiObject = {
-		"Position",
-		"Size",
-		"Visible",
-		"Rotation",
-		"BackgroundColor3",
-		"BackgroundTransparency",
-		"AnchorPoint",
-		"BorderSizePixel",
-		"ZIndex",
-		"BorderColor3",
-		"ClipsDescendants"
-	},
-	Part = {"Shape"},
-	SpawnLocation = {
-		"Enabled",
-		"Duration",
-		"AllowTeamChangeOnTouch",
+	Team = {
+		"AutoAssignable",
 		"TeamColor"
 	},
-	VehicleSeat = {
-		"Disabled",
-		"HeadsUpDisplay",
-		"MaxSpeed",
-		"Steer",
-		"SteerFloat",
-		"Throttle",
-		"ThrottleFloat",
-		"Torque",
-		"TurnSpeed"
-	},
-	Seat = {
-		"Disabled"
-	},
-	WedgePart = {},
-	CornerWedgePart = {},
-	TrussPart = {},
-	Weld = {},
-	ManualWeld = {},
-	Motor6D = {},
-	Motor = {},
-	Frame = {},
-	ImageLabel = {
-		"Image",
-		"ImageColor3",
-		"ImageRectSize",
-		"ImageTransparency",
-		"ScaleType"
-	},
-	Humanoid = {
-		"DisplayDistanceType",
-		"DisplayName",
-		"HealthDisplayDistance",
-		"HealthDisplayType",
-		"NameDisplayDistance",
-		"NameOcclusion",
-		"RigType",
-		"BreakJointsOnDeath",
-		"RequiresNeck",
-		"Jump",
-		"PlatformStand",
-		"Sit",
-		"MaxHealth",
-		"Health",
-		"HipHeight",
-		"MaxSlopeAngle",
-		"WalkSpeed",
-		"JumpHeight",
-		"JumpPower",
-		"UseJumpPower"
-	},
-	BoolValue = {"Value"},
-	BrickColorValue = {"Value"},
-	IntValue = {"Value"},
-	NumberValue = {"Value"},
-	StringValue = {"Value"},
-	Vector3Value = {"Value"},
-	Sound = {
-		"PlayOnRemove",
-		"SoundId",
-		"Looped",
-		"Pitch",
-		"PlaybackSpeed",
-		"Playing",
-		"TimePosition",
-		"Volume"
-	},
-	BodyColors = {
-		"HeadColor",
-		"LeftArmColor",
-		"LeftLegColor",
-		"RightArmColor",
-		"RightLegColor",
-		"TorsoColor"
+	SurfaceSelection = {
+		"Color3",
+		"Transparency",
+		"TargetSurface",
+		"Visible"
 	},
 	SurfaceLight = {
 		"Angle",
@@ -223,54 +169,336 @@ module.Properties = {
 		"Range",
 		"Shadows"
 	},
+	SpecialMesh = {
+		"MeshId",
+		"MeshType",
+		"Offset",
+		"Scale",
+		"TextureId",
+		"VertexColor"
+	},
+	SpawnLocation = {
+		"AllowTeamChangeOnTouch",
+		"Neutral",
+		"TeamColor",
+		"Duration"
+	},
+	Sparkles = {
+		"SparkleColor"
+	},
+	Sound = {
+		"Looped",
+		"Pitch",
+		"PlaybackSpeed",
+		"Playing",
+		"SoundId",
+		"Volume",
+		"PlayOnRemove"
+	},
+	Smoke = {
+		"Color",
+		"Opacity",
+		"RiseVelocity",
+		"Size"
+	},
+	Sky = {
+		"CelestialBodiesShown",
+		"MoonAngularSize",
+		"MoonTextureId",
+		"SkyboxBk",
+		"SkyboxDn",
+		"SkyboxFt",
+		"SkyboxLf",
+		"SkyboxRt",
+		"SkyboxUp",
+		"StarCount",
+		"SunAngularSize",
+		"SunTextureId"
+	},
+	Skin = {"SkinColor"},
+	ShirtGraphic = {
+		"Color3",
+		"Graphic"
+	},
+	Shirt = {
+		"Color3",
+		"ShirtTemplate"
+	},
+	SelectionBox = {
+		"Color3",
+		"LineThickness",
+		"SurfaceColor3",
+		"SurfaceTransparency",
+		"Transparency",
+		"Visible"
+	},
+	Seat = {"Disabled"},
+	Script = {
+		"Disabled",
+		"Enabled"
+	},
+	RocketPropulsion = {
+		"CartoonFactor",
+		"TargetOffset",
+		"TargetRadius",
+		"MaxSpeed",
+		"MaxThrust",
+		"ThrustD",
+		"ThrustP",
+		"MaxTorque",
+		"TurnD",
+		"TurnP"
+	},
 	PointLight = {
 		"Brightness",
 		"Color",
 		"Enabled",
 		"Range",
 		"Shadows"
-	}
-}
-
-addToTable(module.Properties.Part,module.Properties.BasePart)
-addToTable(module.Properties.SpawnLocation,module.Properties.BasePart)
-addToTable(module.Properties.VehicleSeat,module.Properties.BasePart)
-addToTable(module.Properties.Seat,module.Properties.BasePart)
-addToTable(module.Properties.WedgePart,module.Properties.BasePart)
-addToTable(module.Properties.CornerWedgePart,module.Properties.BasePart)
-addToTable(module.Properties.TrussPart,module.Properties.BasePart)
-addToTable(module.Properties.Weld,module.Properties.JointInstance)
-addToTable(module.Properties.ManualWeld,module.Properties.JointInstance)
-addToTable(module.Properties.Motor6D,module.Properties.JointInstance)
-addToTable(module.Properties.Motor,module.Properties.JointInstance)
-addToTable(module.Properties.Frame,module.Properties.GuiObject)
-addToTable(module.Properties.ImageLabel,module.Properties.GuiObject)
-
-module.InstanceProperties = {
-	["Instance"] = {
-		"Parent"
 	},
-	JointInstance = {
-		"Part0",
-		"Part1",
-		"Enabled"
+	Pants = {
+		"Color3",
+		"PantsTemplate"
 	},
-	Model = {
-		"PrimaryPart"
-	},
-	Weld = {},
-	ManualWeld = {},
 	Motor6D = {
 		"CurrentAngle",
 		"DesiredAngle",
 		"MaxVelocity"
 	},
-	Motor = {},
-	ObjectValue = {"Value"}
+	Motor = {
+		"CurrentAngle",
+		"DesiredAngle",
+		"MaxVelocity"
+	},
+	Message = {"Text"},
+	LocalScript = {
+		"Disabled",
+		"Enabled"
+	},
+	Humanoid = {
+		"Health",
+		"JumpPower",
+		"MaxHealth",
+		"WalkSpeed",
+		"Jump",
+		"PlatformStand",
+		"Sit"
+	},
+	Hint = {"Text"},
+	Hat = {
+		"AttachmentForward",
+		"AttachmentPos",
+		"AttachmentRight",
+		"AttachmentUp"
+	},
+	FlagStand = {"TeamColor"},
+	Flag = {
+		"GripForward",
+		"GripPos",
+		"GripRight",
+		"GripUp",
+		"ToolTip",
+		"TeamColor",
+		"TextureId",
+		"CanBeDropped",
+		"RequiresHandle",
+		"Enabled"
+	},
+	Fire = {
+		"Color",
+		"Heat",
+		"SecondaryColor",
+		"Size"
+	},
+	Dialog = {
+		"ConversationDistance",
+		"GoodbyeDialog",
+		"InUse",
+		"InitialPrompt",
+		"Purpose",
+		"Tone"
+	},
+	DialogChoice = {
+		"GoodbyeDialog",
+		"ResponseDialog",
+		"UserDialog"
+	},
+	Decal = {
+		"Color3",
+		"Texture",
+		"Transparency",
+		"Face"
+	},
+	CylinderMesh = {
+		"Offset",
+		"Scale",
+		"VertexColor"
+	},
+	ClickDetector = {"MaxActivationDistance"},
+	CharacterMesh = {
+		"BaseTextureId",
+		"BodyPart",
+		"MeshId",
+		"OverlayTextureId"
+	},
+	BodyVelocity = {
+		"P",
+		"maxForce",
+		"velocity",
+		"MaxForce",
+		"Velocity"
+	},
+	BodyThrust = {
+		"force",
+		"location",
+		"Force",
+		"Location"
+	},
+	BodyPosition = {
+		"D",
+		"P",
+		"maxForce",
+		"position",
+		"MaxForce",
+		"Position"
+	},
+	BodyGyro = {
+		"D",
+		"P",
+		"maxTorque",
+		"MaxTorque",
+		"CFrame"
+	},
+	BodyForce = {
+		"force",
+		"Force"
+	},
+	BodyColors = {
+		"HeadColor",
+		"LeftArmColor",
+		"LeftLegColor",
+		"RightArmColor",
+		"RightLegColor",
+		"TorsoColor"
+	},
+	BodyAngularVelocity = {
+		"P",
+		"angularvelocity",
+		"maxTorque",
+		"AngularVelocity",
+		"MaxTorque"
+	},
+	BlockMesh = {
+		"Offset",
+		"Scale",
+		"VertexColor"
+	},
+	Animation = {"AnimationId"},
+	Accoutrement = {
+		"AttachmentForward",
+		"AttachmentPos",
+		"AttachmentRight",
+		"AttachmentUp"
+	},
+	Accessory = {
+		"AttachmentPoint"
+	},
+	Attachment = {
+		"Visible",
+		"Position",
+		"Orientation",
+		"CFrame"
+	}
 }
-addToTable(module.InstanceProperties.Weld,module.InstanceProperties.JointInstance)
-addToTable(module.InstanceProperties.ManualWeld,module.InstanceProperties.JointInstance)
-addToTable(module.InstanceProperties.Motor6D,module.InstanceProperties.JointInstance)
-addToTable(module.InstanceProperties.Motor,module.InstanceProperties.JointInstance)
+
+local InstanceBaseClasses = {
+	["Instance"] = {"Parent"},
+	["JointInstance"] = {
+		"Part0",
+		"Part1"
+	}
+}
+
+module.InstanceProperties = {
+	SurfaceGui = {"Adornee"},
+	BillboardGui = {"Adornee"},
+	SurfaceSelection = {"Adornee"},
+	SelectionBox = {"Adornee"},
+	RocketPropulsion = {"Target"},
+	ObjectValue = {"Value"},
+	Model = {"PrimaryPart"},
+	Humanoid = {
+		"LeftLeg",
+		"RightLeg",
+		"WalkToPart"
+	},
+}
+
+local InheritedClasses = {
+	TextLabel = {"GuiObject"},
+	TextButton = {"GuiObject","GuiButton"},
+	TextBox = {"GuiObject"},
+	ImageLabel = {"GuiObject"},
+	ImageButton = {"GuiObject","GuiButton"},
+	Frame = {"GuiObject"},
+	Weld = {"JointInstance"},
+	Part = {"BasePart"},
+	Wedge = {"BasePart"},
+	VehicleSeat = {"BasePart"},
+	Vector3Value = {"ValueBase"},
+	Truss = {"BasePart"},
+	StringValue = {"ValueBase"},
+	SpawnLocation = {"BasePart"},
+	Seat = {"BasePart"},
+	NumberValue = {"ValueBase"},
+	Motor6D = {"JointInstance"},
+	Motor = {"JointInstance"},
+	ManualWeld = {"JointInstance"},
+	IntValue = {"ValueBase"},
+	CornerWedge = {"BasePart"},
+	BrickColorValue = {"ValueBase"},
+	BoolValue = {"ValueBase"}
+}
+
+for className,v in pairs(InheritedClasses) do
+	for _,baseClass in pairs(v) do
+		for _,Value in pairs(BaseClasses[baseClass]) do
+			local Table = module.Properties[className]
+			if not Table then
+				module.Properties[className] = {}
+				Table = module.Properties[className]
+			end
+			table.insert(Table,Value)
+		end
+	end
+end
+
+for className,v in pairs(InheritedClasses) do
+	for _,baseClass in pairs(v) do
+		InstanceBaseClass = InstanceBaseClasses[baseClass]
+		if InstanceBaseClass then
+			for _,Value in pairs(InstanceBaseClass) do
+				local Table = module.InstanceProperties[className]
+				if not Table then
+					module.InstanceProperties[className] = {}
+					Table = module.InstanceProperties[className]
+				end
+				table.insert(Table,Value)
+			end
+		end
+	end
+end
+
+for i,v in pairs(module.Properties) do
+	for _,propName in pairs(BaseClasses.Instance) do
+		table.insert(v,propName)
+	end
+end
+
+for i,v in pairs(module.InstanceProperties) do
+	for _,propName in pairs(InstanceBaseClasses.Instance) do
+		table.insert(v,propName)
+	end
+end
 
 return module
