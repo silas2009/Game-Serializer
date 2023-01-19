06d94aed4783c01847f99c2b650894bc2d812f30
silas2009 <97012583+silas2009@users.noreply.github.com>
2023-01-18 19:44:39 -0600

    Update ValueDeserializer.lua

--- Serializer/Values/ValueDeserializer.lua
+++ Serializer/Values/ValueDeserializer.lua
@@ -1,28 +1,28 @@
-local Import = {}
-function Import.Vector3(Value)
+local Deserialize = {}
+function Deserialize.Vector3(Value)
 	return Vector3.new(Value.X, Value.Y, Value.Z)
 end
-function Import.Vector2(Value)
+function Deserialize.Vector2(Value)
 	return Vector2.new(Value.X, Value.Y)
 end
-function Import.EnumItem(Value)
+function Deserialize.EnumItem(Value)
 	return Enum[Value.EnumType][Value.Name]
 end
-function Import.Color3(Value)
+function Deserialize.Color3(Value)
 	return Color3.new(Value.R, Value.G, Value.B)
 end
-function Import.BrickColor(Value)
+function Deserialize.BrickColor(Value)
 	return BrickColor.new(Value.Name)
 end
-function Import.CFrame(Value)
+function Deserialize.CFrame(Value)
 	return CFrame.new(Value.x, Value.y, Value.z, Value.r0, Value.r1, Value.r2, Value.r10, Value.r11, Value.r12,
 		Value.r20, Value.r21, Value.r22)
 end
-function Import.UDim2(Value)
+function Deserialize.UDim2(Value)
 	return UDim2.new(Value.X.Scale,Value.X.Offset,Value.Y.Scale,Value.Y.Offset)
 end
-function Import.UDim(Value)
+function Deserialize.UDim(Value)
 	return UDim.new(Value.Scale,Value.Offset)
 end
 
-return Import
+return Deserialize
