0cd38f9ddafce92a3450efe324bacc67fc009f95
silas2009 <97012583+silas2009@users.noreply.github.com>
2023-01-18 20:19:43 -0600

    Update functions.lua

--- Serializer/Misc/functions.lua
+++ Serializer/Misc/functions.lua
@@ -1,12 +1,14 @@
 local functions = {}
 function functions.recurseTable(tbl,func)
-	for index, value in pairs(tbl) do
-		if typeof(value) == "table" then
-			recurseTable(value)
-		else
-			func(index,value)
+	spawn(function()
+		for index, value in pairs(tbl) do
+			if typeof(value) == "table" then
+				recurseTable(value)
+			else
+				func(index,value)
+			end
 		end
-	end
+	end)
 end
 
 return functions
