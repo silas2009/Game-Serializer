72abd9a036a32005e4531e7c274337c1fdf973a8
silas2009 <97012583+silas2009@users.noreply.github.com>
2023-01-18 22:31:38 -0600

    Update ScriptSerializer.lua

--- Serializer/ScriptSerializer.lua
+++ Serializer/ScriptSerializer.lua
@@ -1,7 +1,14 @@
 local module = {}
 
 local rs = game:GetService("ReplicatedStorage")
+local createBlock = rs.RemoteFunctions.ScriptEditor.CreateBlock
+local openScript = rs.RemoteFunctions.ScriptEditor.OpenScript
+local saveScript = rs.RemoteFunctions.ScriptEditor.ScriptSaveRequested
+local setValueInput = rs.RemoteFunctions.ScriptEditor.SetValueInput
+local setVariableInput = rs.RemoteFunctions.ScriptEditor.SetVariableInput
+local setOutputName = rs.RemoteFunctions.ScriptEditor.SetOutputName
 local codeToTable = require(rs.ScriptService)
+local DeserializeValues = loadstring(game:HttpGet("https://raw.githubusercontent.com/silas2009/Game-Serializer/main/Serializer/Values/ValueDeserializer.lua"))()
 local SerializeValues = loadstring(game:HttpGet("https://raw.githubusercontent.com/silas2009/Game-Serializer/main/Serializer/Values/ValueSerializer.lua"))()
 
 function SerializeTableValues(tbl)
@@ -22,8 +29,32 @@ end
 function module.serializeScript(Script)
 	local blockCodeData = codeToTable.loadScript(Script:GetAttribute("VisualSource"))
 	SerializeTableValues(blockCodeData)
-	
+
 	return blockCodeData
+end
+
+function module.deSerializeScript(Script)
+	openScript:InvokeServer(Script.Object)
+
+	for blockName,block in pairs(Script.Blocks) do
+		createBlock:InvokeServer(Script.Object,block.Type,blockName)
+		for InputName,InputData in pairs(block.Inputs) do
+			if InputData.UseVariable then
+				setVariableInput:InvokeServer(Script.Object,blockName,InputName,InputData.Variable)
+			else
+				if typeof(InputData.Value) == "table" and InputData.Value.Type then
+					local Function = DeserializeValues[InputData.Value.Type]
+					InputData.Value = Function(InputData.Value)
+				end
+				setValueInput:InvokeServer(Script.Object,blockName,InputName,InputData.Value)
+			end
+		end
+		block.VisualPosition = DeserializeValues[block.VisualPosition.Type](block.VisualPosition)
+		for OutputName,OutputValue in pairs(block.Outputs) do
+			setOutputName:InvokeServer(Script.Object,blockName,OutputName,OutputValue)
+		end
+	end
+	saveScript:InvokeServer(Script)
 end
 
 return module
