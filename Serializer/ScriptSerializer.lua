fc2f9b61ced3bd9651963763302e4591d58b099f
silas2009 <97012583+silas2009@users.noreply.github.com>
2023-01-19 15:09:58 -0600

    Update ScriptSerializer.lua

--- Serializer/ScriptSerializer.lua
+++ Serializer/ScriptSerializer.lua
@@ -19,28 +19,43 @@ if game.PlaceId == 5846387555 then
 	setOutputName = rs.RemoteFunctions.ScriptEditor.SetOutputName
 end
 function codeToTable(Script)
-    return converter(compression.decompress(Script))
+	return converter(compression.decompress(Script))
 end
 
 function module.deSerializeScript(Object,ScriptString)
 	local Script = codeToTable(ScriptString)
 	Script.Object = Object
 	openScript:InvokeServer(Script.Object)
+	local done = false
 	
-	for blockName,block in pairs(Script.Blocks) do
-		createBlock:InvokeServer(Script.Object,block.Type,blockName)
-		for InputName,InputData in pairs(block.Inputs) do
-			if InputData.UseVariable then
-				setVariableInput:InvokeServer(Script.Object,blockName,InputName,InputData.Variable)
-			else
-				setValueInput:InvokeServer(Script.Object,blockName,InputName,InputData.Value)
-			end
-		end
-		for OutputName,OutputValue in pairs(block.Outputs) do
-			setOutputName:InvokeServer(Script.Object,blockName,OutputName,OutputValue)
-		end
+	local amount = 0
+	for i,v in pairs(Script.Blocks) do
+		amount += amount
 	end
-	saveScript:InvokeServer(Script)
+	local current = 0
+	for blockName,block in pairs(Script.Blocks) do
+		spawn(function()
+			current += 1
+			createBlock:InvokeServer(Script.Object,block.Type,blockName)
+			for InputName,InputData in pairs(block.Inputs) do
+				if InputData.UseVariable then
+					setVariableInput:InvokeServer(Script.Object,blockName,InputName,InputData.Variable)
+				else
+					setValueInput:InvokeServer(Script.Object,blockName,InputName,InputData.Value)
+				end
+			end
+			for OutputName,OutputValue in pairs(block.Outputs) do
+				setOutputName:InvokeServer(Script.Object,blockName,OutputName,OutputValue)
+			end
+			if current == amount then
+				done = true
+			end
+		end)
+	end
+	spawn(function()
+		repeat task.wait() until done
+		saveScript:InvokeServer(Script)
+	end)
 end
 
 return module
