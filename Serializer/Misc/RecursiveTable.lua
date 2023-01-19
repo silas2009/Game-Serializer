8069ce04ad6fef718891937b4c226b0554602bdc
silas2009 <97012583+silas2009@users.noreply.github.com>
2023-01-18 20:15:55 -0600

    Update RecursiveTable.lua

--- Serializer/Misc/RecursiveTable.lua
+++ Serializer/Misc/RecursiveTable.lua
@@ -1,11 +1,9 @@
 return function recurseTable(tbl,func)
-	spawn(function()
-		for index, value in pairs(tbl) do
-			if typeof(value) == "table" then
-				recurseTable(value)
-			else
-				func(index,value)
-			end
+	for index, value in pairs(tbl) do
+		if typeof(value) == "table" then
+			recurseTable(value)
+		else
+			func(index,value)
 		end
-	end)
+	end
 end
