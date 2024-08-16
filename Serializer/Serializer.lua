d3ce87b3a42f1f31a82b0992321608c04e26c398
silas2009 <97012583+silas2009@users.noreply.github.com>
2024-08-16 17:52:10 -0500

    Update Serializer.lua

--- Serializer/Serializer.lua
+++ Serializer/Serializer.lua
@@ -1,38 +1,41 @@
-local module = {}
+local module = {modules={},["settings"]={}}
 
-local blacklist = {
-	"Terrain","Camera","TouchTransmitter"
+local http = game:GetService("HttpService")
+
+module.settings.blacklist = {"Terrain","Camera","TouchTransmitter"}
+module.settings.allowedServices = {"Workspace","Lighting","StarterGui","StarterPack","Teams","SoundService"}
+
+module.modules.properties = require(script.Properties)
+local props = module.modules.properties.Properties
+local InstanceProps = module.modules.properties.InstanceProperties
+module.modules.scriptSerializer = require(script.ScriptSerializer)
+module.modules.values = {
+	valueSerializer = require(script.Values.ValueSerializer),
+	valueDeserializer = require(script.Values.ValueDeserializer)
+}
+module.modules.textures = {
+	surfaceTextures = require(script.Textures.RetroStudioSurfaceTextures),
+	materialTextures = require(script.Textures.RetroStudioMaterialTextures)
+}
+module.modules.util = {
+	guis = require(script.Util.GUIs),
+	building = require(script.Util.Building),
+	functions = require(script.Util.Functions)
+}
+module.modules.resources = {
+	classIcons = require(script.Resources.ClassIcons),
+	classWhitelist = require(script.Resources.ClassWhitelist)
 }
 
-local AllowedServices = {
-	"Workspace","Lighting","StarterGui","StarterPack","Teams","SoundService"
-}
-
-local propsTable = loadstring(game:HttpGet("https://raw.githubusercontent.com/silas2009/Game-Serializer/main/Serializer/Properties.lua"))()
-local props = propsTable.Properties
-local InstanceProps = propsTable.InstanceProperties
-local SerializeValues = loadstring(game:HttpGet("https://raw.githubusercontent.com/silas2009/Game-Serializer/main/Serializer/Values/ValueSerializer.lua"))()
-local TextureSurfaces = loadstring(game:HttpGet("https://raw.githubusercontent.com/silas2009/Game-Serializer/main/Serializer/Textures/RetroStudioSurfaceTextures.lua"))()
-local TextureMaterials = loadstring(game:HttpGet("https://raw.githubusercontent.com/silas2009/Game-Serializer/main/Serializer/Textures/RetroStudioMaterialTextures.lua"))()
-
-function convertId(assetId)
-	local starts = {
-		"http://www.roblox.com/asset?id="
-	}
-	for i,v in pairs(starts) do
-		if string.sub(assetId,0,#v) == v then
-			return "rbxassetid://" .. string.sub(assetId,#v+1,#assetId)
-		end
-	end
-	return assetId
-end
+local convertId = module.modules.util.functions.convertId
+local build = module.modules.util.building
 
 function module.Serialize(Object,json)
 	local defaultProperties = {}
 	local objs
 	if Object == game then
 		objs = {}
-		for i,v in pairs(AllowedServices) do
+		for i,v in pairs(module.settings.allowedServices) do
 			local service = game:GetService(v)
 			if service then
 				for _,obj in pairs(service:GetDescendants()) do
@@ -52,7 +55,7 @@ function module.Serialize(Object,json)
 	local partsWithSurfaces = {}
 	for _,v in ipairs(objs) do
 		if v:IsA("Texture") or v:IsA("Decal") then
-			for _,data in pairs(TextureSurfaces) do
+			for _,data in pairs(module.modules.textures.surfaceTextures) do
 				if data.Texture == v.Texture then
 					table.insert(fakeSurfaces,v)
 					local part = {Part = v.Parent}
@@ -61,7 +64,7 @@ function module.Serialize(Object,json)
 					break
 				end
 			end
-			for _,data in pairs(TextureMaterials) do
+			for _,data in pairs(module.modules.textures.materialTextures) do
 				if data.Texture == v.Texture then
 					if not table.find(fakeSurfaces,v) then
 						table.insert(fakeSurfaces,v)
@@ -72,17 +75,8 @@ function module.Serialize(Object,json)
 		end
 	end
 	local blackListedObjs = {}
-	local times = 0
-	local progress = 0
 	for _,v in ipairs(objs) do
-		if not table.find(blacklist,v.ClassName) and not table.find(fakeSurfaces,v) and not table.find(blackListedObjs,v) then
-			times += 1
-			progress += 1
-			-- print((progress/#objs)*100 .. "/" .. 100)
-			if times >= 25 then
-				task.wait(0.1)
-				times = 0
-			end
+		if not table.find(module.settings.blacklist,v.ClassName) and not table.find(fakeSurfaces,v) and not table.find(blackListedObjs,v) then
 			local objSerialized = {}
 			local classProp = props[v.ClassName]
 			if classProp then
@@ -97,7 +91,7 @@ function module.Serialize(Object,json)
 				end
 				if surfaceobj then
 					for propName,prop in pairs(surfaceobj) do
-						local Function = SerializeValues[typeof(v[propName])]
+						local Function = module.modules.values.valueSerializer[typeof(v[propName])]
 						if Function then
 							objSerialized[propName] = Function(prop)
 						else
@@ -122,7 +116,7 @@ function module.Serialize(Object,json)
 						if prop ~= "ClassName" and objProperty == defaultValue then
 							continue
 						end
-						local Function = SerializeValues[typeof(objProperty)]
+						local Function = module.modules.values.valueSerializer[typeof(objProperty)]
 						if Function then
 							objSerialized[prop] = Function(objProperty)
 						else
@@ -139,17 +133,14 @@ function module.Serialize(Object,json)
 			table.insert(objTable,v)
 		end
 	end
-	local progress = 0
 	for i,v in ipairs(objsSerialized) do
-		progress += 1
-		print((progress/#objs)*100 .. "/" .. 100)
 		local realObj = objTable[i]
 		local classProp = InstanceProps[v.ClassName]
 		if classProp then
 			for _,prop in pairs(classProp) do
 				local foundObj = table.find(objTable,realObj[prop])
 				v[prop] = {Type = "Instance", ["Instance"] = foundObj}
-				if prop == "Parent" and realObj[prop].Parent == game and table.find(AllowedServices,realObj[prop].ClassName) then
+				if prop == "Parent" and realObj[prop].Parent == game and table.find(module.settings.allowedServices,realObj[prop].ClassName) then
 					v[prop].Instance = realObj[prop].ClassName
 				end
 			end
@@ -157,9 +148,79 @@ function module.Serialize(Object,json)
 	end
 
 	if json then
-		objsSerialized = game:GetService("HttpService"):JSONEncode(objsSerialized)
+		objsSerialized = http:JSONEncode(objsSerialized)
 	end
 	return objsSerialized
+end
+
+function module.Deserialize(serialized,updateFunction)
+	if typeof(serialized) == "string" then serialized = http:JSONDecode(serialized) end
+
+	local exportFolder = build.createObj("Folder", game:GetService("Lighting"))
+	build.setProperty(exportFolder,"Name","Export")
+	local unparentedFolder = build.createObj("Folder", exportFolder)
+	build.setProperty(unparentedFolder,"Name","Unparented")
+
+	local instances = {}
+
+	for id,v in ipairs(serialized) do
+		local success,instance = pcall(function()
+			return build.createObj(v.ClassName, unparentedFolder)
+		end)
+		if success and instance then
+			if updateFunction then
+				updateFunction(id,#serialized,"Building")
+			end
+			instances[tostring(id)] = instance
+		end
+	end
+
+	for id,v in pairs(serialized) do
+		if instances[tostring(id)] then
+			local instance = instances[tostring(id)]
+			if updateFunction then
+				updateFunction(id,#serialized,"Setting properties")
+			end
+			pcall(function()
+				if instance:IsA("BasePart") then
+					build.setProperty(instance,"FormFactor","Custom")
+				end
+			end)
+			for propName,propValue in pairs(v) do
+				pcall(function()
+					if typeof(propValue) == "table" then
+						if propValue.Type ~= "Instance" then
+							local valueFunction = module.modules.values.valueDeserializer[propValue.Type]
+							if valueFunction then
+								build.setProperty(instance,propName,valueFunction(propValue))
+							end
+						else
+							if typeof(propValue.Instance) == "number" then
+								local otherInstance = instances[tostring(propValue.Instance)]
+								if not otherInstance and propName == "Parent" then
+									build.setProperty(instance,"Parent",exportFolder)
+								else
+									build.setProperty(instance,propName,otherInstance)
+								end
+							elseif typeof(propValue.Instance) == "string" then
+								local otherInstance = exportFolder
+								if otherInstance then
+									build.setProperty(instance,propName,otherInstance)
+								end
+							end
+							if not instance.Parent then
+								build.setProperty(instance,"Parent",exportFolder)
+							end
+						end
+					else
+						build.setProperty(instance,propName,propValue)
+					end
+				end)
+			end
+		end
+	end
+
+	return exportFolder
 end
 
 return module
