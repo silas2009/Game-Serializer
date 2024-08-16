d7b6960910331dfad0a5574cf30af3e894c4509c
silas2009 <97012583+silas2009@users.noreply.github.com>
2024-08-16 18:22:51 -0500

    Update ScriptSerializer.lua

--- Serializer/ScriptSerializer.lua
+++ Serializer/ScriptSerializer.lua
@@ -1,56 +1,27 @@
-local module = {}
+local isStudio = game.PlaceId == 5846387555
 
 local rs = game:GetService("ReplicatedStorage")
-local ScriptService = game:GetService("ReplicatedStorage").ScriptService
+local ScriptService = rs.ScriptService
 local compression = require(ScriptService.Compression)
 local converter = require(ScriptService.LoadModules:FindFirstChild("0000000000000001"))
-local createBlock
-local openScript
-local saveScript
-local setValueInput
-local setVariableInput
-local setOutputName
-local setValueType
-if game.PlaceId == 5846387555 then
-	local remoteFunctions = rs.RemoteFunctions
-	createBlock = remoteFunctions.ScriptEditor.CreateBlock
-	openScript = remoteFunctions.ScriptEditor.OpenScript
-	saveScript = remoteFunctions.ScriptEditor.ScriptSaveRequested
-	setValueInput = remoteFunctions.ScriptEditor.SetValueInput
-	setVariableInput = remoteFunctions.ScriptEditor.SetVariableInput
-	setOutputName = remoteFunctions.ScriptEditor.SetOutputName
-	setValueType =  remoteFunctions.ScriptEditor.SetValueType
-end
+local remoteFunctions = rs:WaitForChild("RemoteFunctions",2)
+local createBlock = remoteFunctions and remoteFunctions.ScriptEditor.CreateBlock
+local openScript = remoteFunctions and remoteFunctions.ScriptEditor.OpenScript
+local saveScript = remoteFunctions and remoteFunctions.ScriptEditor.ScriptSaveRequested
+local setValueInput = remoteFunctions and remoteFunctions.ScriptEditor.SetValueInput
+local setVariableInput = remoteFunctions and remoteFunctions.ScriptEditor.SetVariableInput
+local setOutputName = remoteFunctions and remoteFunctions.ScriptEditor.SetOutputName
+local setValueType = remoteFunctions and remoteFunctions.ScriptEditor.SetValueType
+
 function codeToTable(Script)
 	return converter(compression.decompress(Script))
 end
-function stringify(v, spaces, usesemicolon, depth)
-	if type(v) ~= 'table' then
-		return tostring(v)
-	elseif not next(v) then
-		return '{}'
-	end
 
-	spaces = spaces or 4
-	depth = depth or 1
-
-	local space = (" "):rep(depth * spaces)
-	local sep = usesemicolon and ";" or ","
-	local concatenationBuilder = {"{"}
-
-	for k, x in next, v do
-		table.insert(concatenationBuilder, ("\n%s[%s] = %s%s"):format(space,type(k)=='number'and tostring(k)or('"%s"'):format(tostring(k)), stringify(x, spaces, usesemicolon, depth+1), sep))
-	end
-
-	local s = table.concat(concatenationBuilder)
-	return ("%s\n%s}"):format(s:sub(1,-2), space:sub(1, -spaces-1))
-end
-
-function module.deSerializeScript(Object,ScriptString)
+return function(Object,ScriptString)
 	local Script = codeToTable(ScriptString)
 	Script.Object = Object
 	openScript:InvokeServer(Script.Object)
-	setclipboard(stringify(Script.Blocks,_,false))
+	--setclipboard(stringify(Script.Blocks,_,false))
 	for blockName,block in pairs(Script.Blocks) do
 		createBlock:InvokeServer(Script.Object,block.Type,blockName)
 		for InputName,InputData in pairs(block.Inputs) do
@@ -67,5 +38,3 @@ function module.deSerializeScript(Object,ScriptString)
 	end
 	saveScript:InvokeServer(Script)
 end
-
-return module
