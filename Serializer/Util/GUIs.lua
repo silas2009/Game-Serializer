98bd0d32bbeadc73e9d403210de20314575df4c8
silas2009 <97012583+silas2009@users.noreply.github.com>
2024-08-16 22:52:38 -0500

    Update GUIs.lua

--- Serializer/Util/GUIs.lua
+++ Serializer/Util/GUIs.lua
@@ -1,6 +1,6 @@
 local module = {}
 
-local classIcons = require(script.Parent.Parent.Resources.ClassIcons)
+local classIcons = loadstring(game:HttpGet("https://raw.githubusercontent.com/silas2009/Game-Serializer/main/Serializer/Resources/ClassIcons.lua"))()
 
 function module.makeInstanceButton(parent,instance)
 	local a=Instance.new"Frame"
