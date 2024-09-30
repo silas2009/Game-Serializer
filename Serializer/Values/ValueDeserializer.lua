48e2f2c9f21a02fece83985914ad7fc4bc2170ac
silas2009 <97012583+silas2009@users.noreply.github.com>
2024-09-30 17:45:14 -0500

    Update ValueDeserializer.lua

--- Serializer/Values/ValueDeserializer.lua
+++ Serializer/Values/ValueDeserializer.lua
@@ -1,28 +1,21 @@
 local Deserialize = {}
 function Deserialize.Vector3(Value)
-	return Vector3.new(Value.X, Value.Y, Value.Z)
+	return Vector3.new(Value.X or Value.x, Value.Y or Value.y, Value.Z or Value.z)
 end
 function Deserialize.Vector2(Value)
-	return Vector2.new(Value.X, Value.Y)
+	return Vector2.new(Value.X or Value.x, Value.Y or Value.y)
 end
 function Deserialize.EnumItem(Value)
 	return Enum[Value.EnumType][Value.Name]
 end
 function Deserialize.Color3(Value)
-	return Color3.new(Value.R, Value.G, Value.B)
+	return Color3.new(Value.R or Value.r, Value.G or Value.g, Value.B or Value.b)
 end
 function Deserialize.BrickColor(Value)
 	return BrickColor.new(Value.Name)
 end
 function Deserialize.CFrame(Value)
-	for name,value in pairs(Value) do
-		if typeof(name) == "string" then
-			Value[name:upper()] = value
-			Value[name] = nil
-		end
-	end
-	return CFrame.new(Value.X, Value.Y, Value.Z, Value.R00 or Value.R0, Value.R01 or Value.R1, Value.R02 or Value.R2, Value.R10, Value.R11, Value.R12,
-		Value.R20, Value.R21, Value.R22)
+	return CFrame.new(Value.X or Value.x, Value.Y or Value.y, Value.Z or Value.z, Value.R00 or Value.R0 or Value.r0, Value.R01 or Value.R1 or Value.r1, Value.R02 or Value.R2 or Value.r2, Value.R10 or Value.r10, Value.R11 or Value.r11, Value.R12 or Value.r12, Value.R20 or Value.r20, Value.R21 or Value.r21, Value.R22 or Value.r22)
 end
 function Deserialize.UDim2(Value)
 	return UDim2.new(Value.X.Scale,Value.X.Offset,Value.Y.Scale,Value.Y.Offset)
